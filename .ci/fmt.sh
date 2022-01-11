b4ac1d0f2289788db0f18bb90302af498782e204
Jorge <jgomezve@cisco.com>
2022-01-11 11:02:59 +0100

    Change fmt script

--- .ci/fmt.sh
+++ .ci/fmt.sh
@@ -3,8 +3,14 @@
 # Code format check
 GO_FILES=$(find . -name '*.go' | grep -v /vendor/ )
 
-gofmt -d ${GO_FILES}
+unformatted=$(gofmt -l $GO_FILES)
+[ -z "$unformatted" ] && exit 0
 
-if [ "$(gofmt ${GO_FILES} | wc -l)" -gt 0 ]; then
-     exit 1
-fi
+# Some files are not gofmt'd. Print message and fail.
+
+echo >&2 "Go files must be formatted with gofmt. Please run:"
+for fn in $unformatted; do
+    echo >&2 "  gofmt -w $fn"
+done
+
+exit 1
